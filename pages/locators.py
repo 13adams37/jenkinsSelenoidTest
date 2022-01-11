8742e2b78b362112f1b69489acc05d387ed62996
13adams37 <76618509+13adams37@users.noreply.github.com>
2022-01-11 12:58:25 +0300

    Updated locators, Added login page tests
    Created locators for loginPage

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_PASS = (By.CSS_SELECTOR, '#id_login-password')
+    LOGIN_BUTTON = (By.XPATH, '//button[@name="login_submit"]')
+    REG_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REG_PASS = (By.CSS_SELECTOR, '#id_registration-password1')
+    REG_RPASS = (By.CSS_SELECTOR, '#id_registration-password2')
+    REG_BUTTON = (By.XPATH, '//button[@name="registration_submit"]')
