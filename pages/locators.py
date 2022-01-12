d49c0f73288efc4dc37e8785586d4011cc2bf1bd
13adams37 <76618509+13adams37@users.noreply.github.com>
2022-01-12 21:45:22 +0300

    Update locators, and etc
    New locators for product page

--- pages/locators.py
+++ pages/locators.py
@@ -13,3 +13,12 @@ class LoginPageLocators:
     REG_PASS = (By.CSS_SELECTOR, '#id_registration-password1')
     REG_RPASS = (By.CSS_SELECTOR, '#id_registration-password2')
     REG_BUTTON = (By.XPATH, '//button[@name="registration_submit"]')
+
+
+class ProductPageLocators:
+    PRODUCT_NAME = (By.XPATH, '//div[@class="col-sm-6 product_main"]/h1')
+    PRODUCT_PRICE = (By.XPATH, '//div[@class="col-sm-6 product_main"]/p[@class="price_color"]')
+    PRODUCT_ADDTOCARTBUTTON = (By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+    PRODUCT_ALERT_NAME = (By.XPATH, '(//div[@class="alert alert-safe alert-noicon alert-success  fade in"]/div/strong)[1]')
+    PRODUCT_ALERT_PRICE = (By.XPATH, '//div[@class="alertinner "]/p/strong')
+    PRODUCT_CART_PRICE = (By.XPATH, '//div[@class="basket-mini pull-right hidden-xs"]')
