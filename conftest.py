0fdd7f5d114541292f77619b635ac5655a2a12d8
13adams37 <76618509+13adams37@users.noreply.github.com>
2022-05-05 16:38:05 +0300

    Added igrore for DepricationWarnings (Desired Capabilities)

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as Chrome_Options
 from selenium.webdriver.firefox.options import Options as Firefox_Options
-from selenium.webdriver import DesiredCapabilities
 
 chrome_capabilities = {
     "browserName": "chrome",
@@ -31,21 +30,13 @@ def browser(request):
     if browser_name == "chrome":
         chrome_options.add_experimental_option('prefs', {'intl.accept_languages': language})
 
-        capabilities = DesiredCapabilities.CHROME.copy()
-        capabilities['browserName'] = 'chrome'
-        capabilities['browserVersion'] = '99.0'
-        capabilities['selenoid:options'] = {"enableVNC": True, "enableVideo": False}
-
         # seleniumIntegrated
-        browser = webdriver.Chrome(options=chrome_options)
+        # browser = webdriver.Chrome(options=chrome_options)
 
         # selenoidRemote
         browser = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
-                                   desired_capabilities=capabilities,
+                                   desired_capabilities=chrome_capabilities,
                                    options=chrome_options)
-
-        # browser = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
-        #                            options=chrome_options)
     elif browser_name == "firefox":
         firefox_options.set_preference("intl.accept_languages", language)
         browser = webdriver.Firefox(options=firefox_options)
